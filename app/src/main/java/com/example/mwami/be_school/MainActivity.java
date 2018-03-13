191aa5effbdc6374f40754146dfe9558efb288e4
kigoryi <mugishaghislain1992@gmail.com>
2018-03-13 13:07:20 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mwami/be_school/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mwami.be_school;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/mwami/be_school/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.mwami.be_school;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent =  new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.finish();
+                SplashActivity.this.startActivity(mainIntent);
+            }
+        },2000);
+
+    }
+}
