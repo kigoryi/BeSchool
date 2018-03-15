e17bf0ae82a7c08edcaa78c4c7250d97b4f71232
kigoryi <mugishaghislain1992@gmail.com>
2018-03-16 01:53:37 +0200

    last update

--- app/src/main/java/com/example/mwami/be_school/MainActivity.java
+++ app/src/main/java/com/example/mwami/be_school/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.mwami.be_school;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewFlipper viewF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
         setContentView(R.layout.activity_main);
+
+        viewF=findViewById(R.id.viewF);
+        viewF.setInAnimation(this,R.anim.fade_in);
+        viewF.setOutAnimation(this,R.anim.fade_out);
+        viewF.showNext();
+
     }
 }
